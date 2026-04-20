aaf0609125831e7a05e224e0bd5434f851c9f904
123 <a123@MacBook-Pro.local>
2026-04-20 17:38:25 +0300

    fix container layout alignment

--- src/components/site/Container.tsx
+++ src/components/site/Container.tsx
@@ -4,8 +4,10 @@ export default function Container({
   children: React.ReactNode;
 }) {
   return (
-    <div className="px-[20px] pt-[40px] pb-[40px]">
-      {children}
+    <div className="w-full flex justify-start">
+      <div className="w-full max-w-[1200px] px-[20px] pt-[40px] pb-[40px]">
+        {children}
+      </div>
     </div>
   );
 }
